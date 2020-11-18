6c46b8109ea69c47ecf5b48a4e4899c42dc3e8a8
laitooo <killuasan25@gmail.com>
2020-11-18 19:49:00 +0200

    starting main list

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,12 +19,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyList extends StatelessWidget {
   List<Record> records = List<Record>();
 
-
-  MyList(){
+  MyList() {
     records.add(Record(0, "record1", 2, 20.0, 30.0, -10.0));
     records.add(Record(1, "record2", 3, 0.0, 14.0, -14.0));
     records.add(Record(2, "record3", 4, 40.0, 30.0, 10.0));
@@ -36,38 +33,69 @@ class MyList extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-          appBar: AppBar(
-            title: Text("Records"),
-          ),
-          body: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: records.length,
-              itemBuilder: (BuildContext context, int index) {
-                return _buildItemsForListView(context, index);
-              }
-          )
-        );
+        appBar: AppBar(
+          title: Text("My Wallet"),
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: records.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildItemsForListView(context, index);
+            }));
   }
 
   Container _buildItemsForListView(BuildContext context, int index) {
     return Container(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Text(records[index].name),
-          SizedBox(height: 10.0,),
-          Row(
-            children: [
-              Text("credit : " + records[index].credit.toString()),
-              SizedBox(width: 20.0,),
-              Text("credit : " + records[index].credit.toString()),
-              SizedBox(width: 20.0,),
-              Text("credit : " + records[index].credit.toString()),
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 2.0,
+                spreadRadius: 0.0,
+                offset: Offset(2.0, 2.0), // shadow direction: bottom right
+              )
             ],
-          )
-        ],
-      )
-    );
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        margin: EdgeInsets.all(10.0),
+        padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              records[index].name,
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.white,
+                  child: Text("credit : " + records[index].credit.toString()),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.white,
+                  child: Text("credit : " + records[index].credit.toString()),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.white,
+                  child: Text("credit : " + records[index].credit.toString()),
+                ),
+              ],
+            )
+          ],
+        ));
   }
-
 }
