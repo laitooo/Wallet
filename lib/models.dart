752e3a5efa829a789cd1f2a7cc917f2be50373e3
laitooo <killuasan25@gmail.com>
2020-11-18 19:16:11 +0200

    starting main list

---
+++ lib/models.dart
@@ -0,0 +1,12 @@
+
+class Record {
+  int id;
+  String name;
+  int currency;
+  double credit;
+  double debit;
+  double balance;
+
+  Record(
+      this.id, this.name, this.currency, this.credit, this.debit, this.balance);
+}
