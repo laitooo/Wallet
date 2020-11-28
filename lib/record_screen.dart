3c057332c97be3c8f2c9f9d4373b48032b4f9a1e
laitooo <killuasan25@gmail.com>
2020-11-28 15:20:49 +0200

    added firebase to project

--- lib/record_screen.dart
+++ lib/record_screen.dart
@@ -21,9 +21,10 @@ class _RecordDetail extends State<RecordDetail> {
         title: Text("Record name"),
       ),
       body: FutureBuilder(
-        //future: loadRecords(),
+        future: loadTransactions(),
         builder: (context, snapshot) {
-          List<Record> list = snapshot.data ?? [];
+          List<Transaction> list = snapshot.data ?? [];
+          list.add(Transaction(3, "From mostaql", "2020/11/09", 24.0, 0));
           return ListView.builder(
               padding: const EdgeInsets.all(8),
               itemCount: list.length,
@@ -46,7 +47,75 @@ class _RecordDetail extends State<RecordDetail> {
     );
   }
 
-  Widget _buildItemsForListView(BuildContext context, int index, Record list) {
-    return Text("test");
+  Container _buildItemsForListView(
+      BuildContext context, int index, Transaction transaction) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2.0,
+              spreadRadius: 0.0,
+              offset: Offset(2.0, 2.0), // shadow direction: bottom right
+            )
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            transaction.title,
+            style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              textBox("credit : \n" + transaction.credit.toString()),
+              textBox("debit : \n" + transaction.debit.toString()),
+              textBox("date : \n" + transaction.date.toString()),
+            ],
+          )
+        ],
+      ),
+    );
   }
+
+  Widget textBox(String text) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2.0,
+              spreadRadius: 0.0,
+              offset: Offset(2.0, 2.0), // shadow direction: bottom right
+            )
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      width: 110.0,
+      height: 80.0,
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.blue, fontSize: 24.0, height: 1.4),
+        ),
+      ),
+    );
+  }
+
+}
+
+loadTransactions() {
+  return null;
 }
